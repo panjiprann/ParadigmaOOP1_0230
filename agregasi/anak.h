a6bd73f42331354aa52b3e22feff3d8e9b898410
panjiprann <panjipranantya20@gmail.com>
2024-05-21 15:55:48 +0700

    menyelesaikan anak

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -5,6 +5,9 @@ class anak {
 public:
     string nama;
     anak(string pNama) :nama(pNama) {
-        
+        cout << "Anak \"" << nama << "\" ada\n";
     }
-}
+    ~anak() {
+        cout << "anak \"" <<nama << "\" tidak ada\n" ;
+};
+#endif
